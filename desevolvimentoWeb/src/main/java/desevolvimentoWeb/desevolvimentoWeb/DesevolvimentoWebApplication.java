c3aa55e73350d44798173704e34a5b6649311187
joaoojohn <joaopedro.cs12@hotmail.com>
2021-11-15 19:48:04 -0300

    first commit

---
+++ desevolvimentoWeb/src/main/java/desevolvimentoWeb/desevolvimentoWeb/DesevolvimentoWebApplication.java
@@ -0,0 +1,13 @@
+package desevolvimentoWeb.desevolvimentoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesevolvimentoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesevolvimentoWebApplication.class, args);
+	}
+
+}
---
+++ desevolvimentoWeb/src/test/java/desevolvimentoWeb/desevolvimentoWeb/DesevolvimentoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package desevolvimentoWeb.desevolvimentoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesevolvimentoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
